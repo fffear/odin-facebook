a5cdf1a4499d266ecf07656ec638388b980d4afd
Kevin Vuong <c_k.k@hotmail.com>
2019-10-02 21:29:35 +1000

    sendgrid addon

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,7 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
@@ -64,6 +64,21 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "odin-facebook_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
+
+   # Setup the mailer config
+   config.action_mailer.delivery_method = :smtp
+   config.action_mailer.perform_deliveries = true
+   host = 'shielded-mesa-75331.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
+   config.action_mailer.smtp_settings = {
+     :user_name => ENV['SENDGRID_USERNAME'],
+     :password => ENV['SENDGRID_PASSWORD'],
+     :domain => 'heroku.com',
+     :address => 'smtp.sendgrid.net',
+     :port => 587,
+     :authentication => :plain,
+     :enable_starttls_auto => true
+   }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
